f742a2a6d060b28969579b2e3f71f3c8c227e0ae
ValentinaCamachoB <Valencamacho777@hotmail.com>
2026-04-01 14:09:23 -0500

    Update VentanaFactura.java

--- Electrodomesticos/src/gui/VentanaFactura.java
+++ Electrodomesticos/src/gui/VentanaFactura.java
@@ -1,37 +1,48 @@
 package gui;
 
-import javax.swing.JFrame;
+import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import java.awt.Font;
 
 
-public class VentanaFactura extends JFrame {
+public class VentanaFactura extends JDialog {
 
-	public VentanaFactura(String factura) {
-		 
-		setTitle("Factura de Compra");
-		setSize(420, 380);
-		setLocationRelativeTo(null);
-		getContentPane().setLayout(null);
-		setAlwaysOnTop(true);
- 
-		JLabel lblTitulo = new JLabel("FACTURA DE COMPRA");
-		lblTitulo.setFont(new Font("Tahoma", Font.BOLD, 18));
-		lblTitulo.setBounds(90, 10, 280, 25);
-		getContentPane().add(lblTitulo);
- 
+	private static VentanaFactura instanciaActual = null;
 
-		JTextArea areaFactura = new JTextArea();
-		areaFactura.setText(factura);
-		areaFactura.setEditable(false);
-		areaFactura.setFont(new Font("Tahoma", Font.PLAIN, 13));
- 
-		JScrollPane scrollPane = new JScrollPane(areaFactura);
-		scrollPane.setBounds(20, 58, 370, 270);
-		getContentPane().add(scrollPane);
+    public static void mostrar(JDialog ventanaPadre, String factura) {
 
-		setVisible(true);
-	}
+        if (instanciaActual != null && instanciaActual.isVisible()) {
+            instanciaActual.dispose();
+        }
+
+        instanciaActual = new VentanaFactura(ventanaPadre, factura);
+        instanciaActual.setVisible(true);
+    }
+
+    private VentanaFactura(JDialog ventanaPadre, String factura) {
+
+        super(ventanaPadre, "Factura de Compra", ModalityType.MODELESS); 
+
+        setSize(420, 380);
+        setLocationRelativeTo(ventanaPadre); 
+        setLayout(null);
+
+        JLabel lblTitulo = new JLabel("FACTURA DE COMPRA");
+        lblTitulo.setFont(new Font("Tahoma", Font.BOLD, 18));
+        lblTitulo.setBounds(90, 10, 280, 25);
+        add(lblTitulo);
+
+        JTextArea areaFactura = new JTextArea();
+        areaFactura.setText(factura);
+        areaFactura.setEditable(false);
+        areaFactura.setFont(new Font("Tahoma", Font.PLAIN, 13));
+
+        JScrollPane scrollPane = new JScrollPane(areaFactura);
+        scrollPane.setBounds(20, 58, 370, 270);
+        add(scrollPane);
+
+        setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+    }
 }
